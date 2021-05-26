4defef456aaaa0b807728c25767f3fad4d096af9
TANVI SHARMA <82575220+TANVISHARMA08@users.noreply.github.com>
2021-05-26 12:54:00 +0530

    UNIT DIGIT

---
+++ 1_10_100.js
@@ -0,0 +1,10 @@
+let digit = parseInt("100");
+console.log(digit)
+if(digit==1)
+    console.log("unit");
+else if(digit==10)
+    console.log("Ten");
+else if(digit==100)
+    console.log("hundred");
+else if(digit==1000)
+    console.log("Thousand");
