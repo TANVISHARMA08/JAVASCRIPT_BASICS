2aa0d758099c9d6e2937fdc0d5506a27094664f5
TANVI SHARMA <82575220+TANVISHARMA08@users.noreply.github.com>
2021-05-26 12:22:09 +0530

    number name

---
+++ number_name.js
@@ -0,0 +1,20 @@
+let digit = Math.floor(Math.random()*10)%10;
+console.log(digit)
+if(digit==1)
+    console.log("one");
+else if(digit==2)
+    console.log("two");
+else if(digit==3)
+    console.log("three");
+else if(digit==4)
+    console.log("four");
+else if(digit==5)
+    console.log("Five");
+else if(digit==6)
+    console.log("Six");
+else if(digit==7)
+    console.log("Seven");
+else if(digit==8)
+    console.log("eight");
+else if(digit==9)
+    console.log("nine");
